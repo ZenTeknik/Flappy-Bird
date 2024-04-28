bacd85c59dfd490f66686c5fdf3037d6a0c0be32
haris78866 <arainharis151@gmail.com>
2024-04-29 01:46:24 +0500

    update

--- Game.hpp
+++ Game.hpp
@@ -8,3 +8,25 @@
 #include"AssestManager.hpp"
 #include"InputManager.hpp"
 
+namespace Sonar
+{
+	struct GameData
+	{
+		stateMachine machine;
+		sf::RenderWindow window;
+		AssestManager assests;
+		InputManager input;
+	};
+	typedef std::shared_ptr<GameData> GameDataRef;
+
+	class  Game {
+	public:
+		Game(int width, int height, std::string title);
+
+	private:
+		const float dt = 1.0f / 60.0f;
+		sf::Clock _clock;
+
+		GameDataRef _data = std::make_shared<GameData>();
+	};
+}
