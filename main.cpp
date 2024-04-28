6b58636c3750027f3b895ee88cb158b91efe65a7
haris78866 <arainharis151@gmail.com>
2024-04-28 23:05:01 +0500

    Update

--- main.cpp
+++ main.cpp
@@ -1,30 +1,5 @@
-#include <SFML/Graphics.hpp>
-#define SCREEN_WIDTH 1000
-#define SCREEN_HEIGHT 1000
+#include<iostream>
 
 int main() {
-    // Rendering the window  with sf(using SFML)  here (awesome SFML) represent  the new opening window name   
-    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Flappy  Bird");
-
-    // loop while window is open 
-    while (window.isOpen()) 
-        // Handle events
-       
-               
-        
-
-        // Update game logic (currently empty)
-
-        // Clear the window
-        window.clear();
-        
-        // Draw SFML objects (currently none) 
-        
-
-
-        // Display what was drawn
-       window.display();
-    
-
-    return 0;
-}
+	return EXIT_SUCCESS;
+} 
